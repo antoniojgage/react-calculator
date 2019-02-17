44716da0850e2a3b7eb25b79474cfb54076d9a2e
Antonio Gage <antoniojgage@gmail.com>
2019-02-17 15:26:34 -0500

    Updates

---
+++ precache-manifest.70003a5bb72f193c6b4b4a1e7089e11b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "305835c2f4a19b830ce2",
+    "url": "/react-calculator/static/js/main.305835c2.chunk.js"
+  },
+  {
+    "revision": "f422771ab806f56cca83",
+    "url": "/react-calculator/static/js/1.f422771a.chunk.js"
+  },
+  {
+    "revision": "305835c2f4a19b830ce2",
+    "url": "/react-calculator/static/css/main.03ccda93.chunk.css"
+  },
+  {
+    "revision": "c0949d066a449db5375fd78c59033ace",
+    "url": "/react-calculator/index.html"
+  }
+];
